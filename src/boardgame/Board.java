4d01706f74419eee65e7058185b526022d52a324
fernandojsmelo <fernandojsmelo@gmail.com>
2023-10-07 18:02:22 -0300

    Handling vaptured pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,19 +1,18 @@
 package boardgame;
 
 public class Board {
-	
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 	
-	public Board(int rows, int columns) { 
+	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
-			throw new BoardException(
-					"Error creating board: there must be at least 1 row and 1 column");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
-		pieces= new  Piece[rows][columns];
+		pieces = new Piece[rows][columns];
 	}
 
 	public int getRows() {
@@ -23,12 +22,12 @@ public class Board {
 	public int getColumns() {
 		return columns;
 	}
-
+	
 	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) {
 			throw new BoardException("Position not on the board");
 		}
-		return pieces[row] [column];
+		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) {
@@ -60,7 +59,7 @@ public class Board {
 	}
 	
 	private boolean positionExists(int row, int column) {
-		return row >= 0 && row < rows && column >= 0  && column < columns;
+		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 	
 	public boolean positionExists(Position position) {
